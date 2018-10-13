7fc623a9ea8449d6871d7a0945446ece60e5e803
Carl Youngblood <carl@youngbloods.org>
2018-10-12 21:39:16 -0700

    Get ownable tests passing

--- test/election.js
+++ test/election.js
@@ -4,7 +4,6 @@ contract('Election tests', (accounts) => {
   let instance;
   let owner = accounts[0];
   let notOwner = accounts[1];
-  let candidate = accounts[2];
 
   beforeEach(async () => {
     instance = await Election.deployed();
@@ -13,9 +12,12 @@ contract('Election tests', (accounts) => {
   it('should not allow anyone but the contract owner to add a candidate', async () => {
     try {
       let result = await instance.addCandidate('George', {from: notOwner});
-      assert.equal(result.toString(), owner);
     } catch (e) {
-      assert.fail(null, null, e.message)
+      assert.equal(e.message, 'VM Exception while processing transaction: revert');
     }
+  });
+
+  it('should allow the contract owner to add a candidate', async () => {
+    let result = await instance.addCandidate('George');
   });
 });
