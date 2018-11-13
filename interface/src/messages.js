af26705a3daeafbdb7bf592d97a1190b81fe5e62
Diogo Perillo <diogo@mainframe.com>
2018-11-13 18:53:03 -0200

    Change favicon

--- interface/src/messages.js
+++ interface/src/messages.js
@@ -1,6 +1,6 @@
 export default {
   en: {
-    dappName: "Mainframe MeetUp - Elections Dapp",
+    dappName: "Mainframe - Election Dapp",
     homeError:
       "Oops, seems like you don't have MetaMask installed or you're connected to the wrong network.",
     admTitle: "Admin Area",
@@ -20,7 +20,7 @@ export default {
     requestAccessButton: "Request Access"
   },
   pt: {
-    dappName: "Mainframe MeetUp - Dapp de Eleições",
+    dappName: "Mainframe - Dapp de Eleições",
     homeError:
       "Eita, parece que você não tem o MetaMask instalado, ou não está conectado na rede correta.",
     admTitle: "Área do Administrador",
